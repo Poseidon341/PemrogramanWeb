de4b13ae5d5fd9634521ab549ebc76fb4ae020ce
Poseidon341 <92150626+Poseidon341@users.noreply.github.com>
2023-10-12 11:25:49 +0700

    Add files via upload

---
+++ Modul 2/Demo 2 afidz/Calculator/script.js
@@ -0,0 +1,25 @@
+let input = document.getElementById("display");
+
+function appendToInput(value) {
+    input.value += value;
+}
+
+function clearInput() {
+    input.value = "";
+}
+
+function calculateResult() {
+    try {
+        input.value = eval(input.value);
+    } catch (error) {
+        input.value = "Error";
+    }
+}
+
+function backspace() {
+    input.value = input.value.slice(0, -1);
+}
+
+function square() {
+    input.value = Math.pow(parseFloat(input.value), 2);
+}
